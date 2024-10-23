280fa9e8186a8c76af0f70ceef282f594267e0f1
星空菌K <18015076816@163.com>
2024-10-23 13:17:12 +0800

    modify some pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld  from './components/HelloWorld.vue';
+
 </script>
 
 <template>
@@ -8,11 +9,11 @@ import { RouterLink, RouterView } from 'vue-router'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="欢迎来到" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">主页</RouterLink>
+        <RouterLink to="/three">模型展示</RouterLink>
       </nav>
     </div>
   </header>
